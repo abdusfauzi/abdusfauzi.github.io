a2e5003cc319166a180f0358cc846278288ee20f
Abdus Fauzi <cola.sced@gmail.com>
2016-05-08 09:42:41 +0800

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -3,4 +3,5 @@ var url = document.querySelector("link[rel='canonical']").getAttribute("href");
 if ( url === undefined || url === null ) {
   url = "http://www.thevocket.com";
 }
+window.stop();
 window.location = url;
