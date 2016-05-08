6c7fb18c080571d366ebcb67e226282cafaa5d4c
Abdus Fauzi <cola.sced@gmail.com>
2016-05-08 09:41:30 +0800

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,6 @@
 // JS
 var url = document.querySelector("link[rel='canonical']").getAttribute("href");
+if ( url === undefined || url === null ) {
+  url = "http://www.thevocket.com";
+}
 window.location = url;
