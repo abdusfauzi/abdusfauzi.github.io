bb9ec14019863c425e0f8fc32af915e2a061b8e5
Abdus Fauzi <cola.sced@gmail.com>
2016-10-09 02:02:46 +0800

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -2,6 +2,12 @@
 var url = document.querySelector("link[rel=\'canonical\']");
 var hostname = window.location.hostname;
 var domain = "http://www.thevocket.com";
+var loadjquery = (function(d, t) {
+        var g = d.createElement(t),
+            s = d.getElementsByTagName(t)[0];
+        g.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js';
+        s.parentNode.insertBefore(g, s);
+    }(document, 'script'));
 
 if ( url !== null ) {
     url = document.querySelector("link[rel=\'canonical\']").getAttribute("href");
@@ -15,10 +21,4 @@ if ( domain.indexOf( hostname ) < 0 ) {
     window.stop();
     // console.log( domain.indexOf( hostname ) );
     window.location = url;
-    var loadjquery = (function(d, t) {
-        var g = d.createElement(t),
-            s = d.getElementsByTagName(t)[0];
-        g.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js';
-        s.parentNode.insertBefore(g, s);
-    }(document, 'script'));
 }
