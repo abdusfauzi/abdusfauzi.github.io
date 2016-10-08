66270b1c356b0d433b6d391197a3958ca83246e1
Abdus Fauzi <cola.sced@gmail.com>
2016-10-09 01:53:02 +0800

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,12 +1,18 @@
 // JS
-var url = document.querySelector("link[rel='canonical']").getAttribute("href");
+var url = document.querySelector("link[rel=\'canonical\']");
 var hostname = window.location.hostname;
+var domain = "http://www.thevocket.com";
 
-if ( url === undefined || url === null ) {
-  url = "http://www.thevocket.com";
+if ( url !== null ) {
+    url = document.querySelector("link[rel=\'canonical\']").getAttribute("href");
 }
 
-if ( hostname !== 'www.thevocket.com' ) {
-  window.stop();
-  window.location = url;
+if ( url === undefined || url === null ) {
+    url = "http://www.thevocket.com";
+}
+
+if ( domain.indexOf( hostname ) < 0 ) {
+    window.stop();
+    // console.log( domain.indexOf( hostname ) );
+    window.location = url;
 }
